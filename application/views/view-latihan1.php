6061c7da625cddacf3b3ee49ae00d5b1f3e97cc1
Ahmadrzki <rizkialazhar7@gmail.com>
2022-09-14 16:37:14 +0700

    Belajar web Programming II

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+   <title> Latihan 1</title>
+</head>
+
+<body>
+   Hallo kawan..yuk kita belajar web programming...<br>
+   nilai1 = <?= $nilai1 ; ?>
+   nilai2 = <?= $nilai2 ; ?>
+   ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+   $nilai1 . " + " .$nilai2 . " = " .$hasil; ?>
+
+</body>
+
+</html>
